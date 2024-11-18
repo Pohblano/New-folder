b25fc746129d88011b989e761ec9dd159c85b3ef
Alberto <alberjasa@gmail.com>
2024-11-18 13:14:14 -0800

    New branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
-def hello_world():
-    return "Hello from Alberto!"
+def greet_person(name):
+		return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    print(hello_world())
+    print(greet_person())
